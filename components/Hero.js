87310e5f2e69e82ffcd49e2887f91ae87117dedd
George.O <george.o@tiomarkets.com>
2023-04-26 16:50:43 +0300

    fixes in hero image

--- components/Hero.js
+++ components/Hero.js
@@ -14,7 +14,7 @@ const Container = styled.div`
 const ImageContainer = styled.div`
   width: 100%;
   height: 100%;
-  background-image: url("https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+  background-image: url("/images/heroBg.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
